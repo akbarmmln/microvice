f06ddf9883ea64ba90de667540fc7cffd8837f80
crazycoding <contoh@crazycoding.com>
2020-03-30 16:09:12 +0700

    update code

--- api/v1/swagger/index.js
+++ api/v1/swagger/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
 
-router.post('/trackingOrder', controller.trackingOrder);
+router.post('/acction/trackingOrder', controller.trackingOrderAction);
+router.post('/star/trackingOrder', controller.trackingOrderStar);
 router.post('/contractList', controller.contractList);
 
 module.exports = router;
