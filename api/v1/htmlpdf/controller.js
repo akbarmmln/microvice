217cb532bc53a8f23d1aa8f2b8d973b5ce8b1348
crazycoding <contoh@crazycoding.com>
2020-05-10 17:05:35 +0700

    update code

--- api/v1/htmlpdf/controller.js
+++ api/v1/htmlpdf/controller.js
@@ -402,7 +402,7 @@ exports.newCreatePDF = async function(req, res){
         const pdf = await page.pdf({
             // format: "A4",
             width: '8.27in',
-            height: '15in',
+            height: '13.5in',
             printBackground: false,
             margin: {
                 top: '10px',
