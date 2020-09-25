0feab0fe444f480473262c65d816acaf854c06be
crazycoding <contoh@crazycoding.com>
2020-09-25 10:49:39 +0700

    update code

--- api/v1/swagger/controller.js
+++ api/v1/swagger/controller.js
@@ -669,8 +669,7 @@ exports.tokenIG = async function(req, res){
   try{
     let instagram = new Instagram(configIG);
     let redirectUri = `http://localhost:${process.env.PORT}/api/v1/swagger/callback-token/`;
-    let a = await instagram.getAuthorizationUrl(redirectUri, { scope: ['basic'] })
-    return res.status(200).json(a);
+    res.redirect(instagram.getAuthorizationUrl(redirectUri, { scope: ['basic'] }));
   }catch(e){
     if (typeof e === 'string') {
       logger.error('error request data', e);
