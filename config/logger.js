02b0c02f6a70854f5b801344b1a80d5823fc6156
crazycoding <contoh@crazycoding.com>
2021-01-08 21:51:21 +0700

    update code

--- config/logger.js
+++ config/logger.js
@@ -1,5 +1,5 @@
 'use strict';
-require('dotenv').config();
+// require('dotenv').config();
 const log4js = require('log4js');
 const logger = log4js.getLogger();
 logger.level = process.env.LOGGING_LEVEL ? process.env.LOGGING_LEVEL : 'debug';
