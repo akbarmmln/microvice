bad2d49edc8ca66877bd15f03583a718c7083edc
crazycoding <contoh@crazycoding.com>
2021-01-10 06:59:19 +0700

    update code

--- config/mailer.js
+++ config/mailer.js
@@ -5,7 +5,7 @@ const utils = require('../utils/utils');
 
 exports.sendGridMailer = async function (mailObject) {
   try {
-    mailObject.from = 'noreply dhousez <noreply@frst.co.id>';
+    mailObject.from = 'noreply dhousez';
     return utils.sendGridMailer(mailObject.from, mailObject.to, mailObject.subject, mailObject.html, mailObject.attachments);
   } catch (e) {
     console.log('failed to send email', e.toString());
