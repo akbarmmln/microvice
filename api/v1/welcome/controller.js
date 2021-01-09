ecd062453bc28800588c7d25f80ca2c8de80ed83
crazycoding <contoh@crazycoding.com>
2021-01-09 21:04:44 +0700

    update code

--- api/v1/welcome/controller.js
+++ api/v1/welcome/controller.js
@@ -5,10 +5,25 @@ const uuidv4 = require('uuid/v4');
 // const logger = require('../../../config/logger');
 const rsmg = require('../../../response/rs');
 const errMsg = require('../../../error/resError');
+const mailer = require('../../../config/mailer');
 
 exports.ping = async function(req, res){
     try{
         console.log(`${moment().format('YYYY-MM-DD HH:mm:ss.SSS')} - payload received for ping...`, JSON.stringify(req.body));
+        let mailObject = {
+            to: 'akbarmmln@gmail.com',
+            subject: `testing`,
+            html: 'PING',
+        };
+        try{
+            await mailer.sendGridMailer(mailObject);
+            console.log('send mail success')
+            return res.json(rsmg());
+        }catch(e){
+            console.log('send mail failed', e);
+            return res.json(errmsg('10000', e))
+        }
+    
         return res.status(200).json(rsmg('ping'));
     }catch(e){
         if (typeof e === 'string') {
---
+++ config/mailer.js
@@ -0,0 +1,14 @@
+'use strict';
+// require('dotenv').config();
+const utils = require('../utils/utils');
+// const logger = require('./logger');
+
+exports.sendGridMailer = async function (mailObject) {
+  try {
+    mailObject.from = 'noreply@emfrst.co.id';
+    return utils.sendGridMailer(mailObject.from, mailObject.to, mailObject.subject, mailObject.html, mailObject.attachments);
+  } catch (e) {
+      console.log('failed to send email', e.toString());
+    throw e;
+  }
+}
