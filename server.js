1501e74df4ed2488bee47c4f851049c372971159
crazycoding <contoh@crazycoding.com>
2021-01-09 13:47:07 +0700

    update code

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-// 'use strict';
-// // require('dotenv').config();
-// const app = require('./app');
-// const moment = require('moment');
-// // const logger = require('./config/logger');
-// const rp = require('request-promise');
-// // const shclr = require('./api/v1/sceduler/controller')
-// // Constants
-// let PORT = process.env.PORT || 8099;
+'use strict';
+// require('dotenv').config();
+const app = require('./app');
+const moment = require('moment');
+// const logger = require('./config/logger');
+const rp = require('request-promise');
+// const shclr = require('./api/v1/sceduler/controller')
+// Constants
+let PORT = process.env.PORT || 8099;
 
-// const server = app.listen(PORT, () => console.log(`API Server started. Listening on port:${PORT}`));
+const server = app.listen(PORT, () => console.log(`API Server started. Listening on port:${PORT}`));
 
-// module.exports = server;
+module.exports = server;
